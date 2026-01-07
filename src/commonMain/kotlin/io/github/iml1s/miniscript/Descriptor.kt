c238269554f9a5efb3afa0336b001c629d553fb5
yami <Yami-RND@skvip.org>
2026-01-07 21:13:54 +0800

    Fix CI/CD: Replace System.arraycopy with common-ready copyInto

--- src/commonMain/kotlin/io/github/iml1s/miniscript/Descriptor.kt
+++ src/commonMain/kotlin/io/github/iml1s/miniscript/Descriptor.kt
@@ -175,15 +175,15 @@ sealed class Descriptor<Pk : MiniscriptKey> {
              // BIP-341: P has implicit even Y.
              val pubKey33 = ByteArray(33)
              pubKey33[0] = 0x02
-             System.arraycopy(pubKey, 0, pubKey33, 1, 32)
+             pubKey.copyInto(pubKey33, destinationOffset = 1, startIndex = 0, endIndex = 32)
              
              val tweak = if (merkleRoot == null) {
                  TapTree.taggedHash("TapTweak", pubKey)
              } else {
                  // tagged_hash("TapTweak", pubKey || merkle_root)
                  val data = ByteArray(pubKey.size + merkleRoot.size)
-                 System.arraycopy(pubKey, 0, data, 0, pubKey.size)
-                 System.arraycopy(merkleRoot, 0, data, pubKey.size, merkleRoot.size)
+                 pubKey.copyInto(data, destinationOffset = 0)
+                 merkleRoot.copyInto(data, destinationOffset = pubKey.size)
                  TapTree.taggedHash("TapTweak", data)
              }
              
